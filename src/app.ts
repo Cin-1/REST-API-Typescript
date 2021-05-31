9c70591013c30a0ad68f60350c47573161d925c4
Cin-1 <cinthiapardos@gmail.com>
2021-05-31 18:35:57 -0300

    app config morgan + cors

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,37 @@
 import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+
+import routerIndex from "./routes";
+import routerUsers from "./routes/users";
 
 class App {
   app: express.Application;
 
   constructor() {
     this.app = express();
+    this.settings();
+    this.middlewares();
+    this.routes();
+  }
+  settings() {
+    this.app.set("port", 4000);
+  }
+
+  middlewares() {
+    this.app.use(morgan("dev"));
+    this.app.use(cors());
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({ extended: false }));
+  }
+
+  routes() {
+    this.app.use(routerIndex);
+    this.app.use("/users", routerUsers);
   }
   start() {
-    this.app.listen(4000, () => {
-      console.log("Server Running on 4000");
+    this.app.listen(this.app.get("port"), () => {
+      console.log("Server Running on", this.app.get("port"));
     });
   }
 }
