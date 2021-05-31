b8acd4b5beb81d9385d08df3ce9693e4d379f983
Cin-1 <cinthiapardos@gmail.com>
2021-05-31 18:36:48 -0300

    app edit

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
 require("dotenv").config();
 import App from "./app";
 import db from "./database";
-import morgan from "morgan";
-import exphbs from "express-handlebars";
 
 db();
 const app = new App();
