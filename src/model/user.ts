f8bacd24aacc0017d5b04398041c242ef28baaec
Cin-1 <cinthiapardos@gmail.com>
2021-06-01 10:07:26 -0300

    timestamps added

--- src/model/user.ts
+++ src/model/user.ts
@@ -1,17 +1,20 @@
+import { now } from "mongoose";
+
 let mongoose = require("mongoose");
 
 let Schema = mongoose.Schema;
 
-let userSchema = new Schema({
-  name: { type: String, required: true },
-  last_name: { type: String, required: true },
-  emails: { type: Array, required: false },
-  phones: { type: Array, required: false },
-  updatedAt: { type: Date, required: false },
-  modifiedAt: { type: Date, required: false },
-  birthday: { type: Date, required: false },
-  password: { type: String, required: false },
-});
+let userSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    last_name: { type: String, required: true },
+    emails: { type: Array, required: true },
+    phones: { type: Array, required: false },
+    birthday: { type: Date, required: false },
+    password: { type: String, required: true },
+  },
+  { timestamps: true }
+);
 
 var User = mongoose.model("User", userSchema);
 
